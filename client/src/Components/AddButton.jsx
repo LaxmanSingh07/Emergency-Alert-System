5999a809887a696cd428286208a26df27c52e941
LAXMAN SINGH <115009552+LaxmanSingh07@users.noreply.github.com>
2023-08-28 00:22:49 +0530

    split

--- client/src/Components/AddButton.jsx
+++ client/src/Components/AddButton.jsx
@@ -5,7 +5,7 @@ const AddButton = ({ onClick }) => {
     <div className="flex justify-center mt-6 mb-6">
       <button
         onClick={onClick}
-        className="text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-700 bg-richblack-800 text-brown-5 hover:text-richblack-800 hover:bg-brown-5"
+        className="text-white px-6 py-3 rounded-md shadow-md bg-richblack-800 text-brown-5 hover:text-richblack-800 hover:bg-brown-5"
       >
         Add Emergency Contact
       </button>
